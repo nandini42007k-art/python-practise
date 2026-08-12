f046bab768592594fcff593c11d3fbae20fff783
nandini42007k-art <nandini4.2007k@gmail.com>
2026-08-12 17:33:09 +0530

    Add files via upload

---
+++ Day1.variables & input.py.py
@@ -0,0 +1,50 @@
+name = "Nandini"
+print(name)
+
+name = "Nandini"
+college = "ACET"
+age = 19
+print(name, college, age)
+
+a = 2
+b = 4
+c = a+b
+print(c)
+
+a = 25
+b = 14
+sub = a-b
+print(sub)
+
+x = int(input("Enter first num:"))
+y = int(input("Enter second num:"))
+print("x=", x*y)
+
+a = 100
+b = 25
+c = a/b
+print(c)
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+print("Reminder = ", a%b)
+
+a = int(input("Enter first num:"))
+b = int(input("Enter second num:"))
+
+a,b = b,a
+
+print("After swapping")
+print("a=", a)
+print("b=", b)
+
+a = int(input("Enter a number:"))
+b = a**2
+
+print("Square =", b)
+
+r = 120
+area = 3.14*r**2
+
+print("Area of circle is", area)
+
