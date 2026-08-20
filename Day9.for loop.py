d993be719abd2cc062ab07fe84972b47339ce3ab
nandini42007k-art <nandini4.2007k@gmail.com>
2026-08-20 17:38:44 +0530

    Add files via upload

---
+++ Day9.for loop.py
@@ -0,0 +1,35 @@
+#print 1-10
+for i in range(1,11):
+    print(i)
+
+#print 10-1
+for i in range(10,0,-1):
+    print(i)
+
+#print even numbers 1-20
+for i in range(2,21,2):
+    print(i)
+
+#print odd numbers 1-20
+for i in range(1,21,2):
+    print(i)
+
+#print multiples of 5
+for i in range(5,51,5):
+    print(i)
+
+for i in range(1,11):
+    print(f"5*{i} = {5*i}")
+
+#print numbers from 1 to N
+n = int(input("Enter N:"))
+for i in range(1,n+1):
+    print(i)
+
+#print squares from 1-10
+for i in range (1,11):
+    print(i**2)
+
+#print cube from 1-10
+for i in range(1,11):
+    print(i**3)
